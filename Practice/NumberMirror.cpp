0ea797a0c315e9f7205562b1d22fdd58e8d546c1
MdImranAli010 <163654831+MdImranAli010@users.noreply.github.com>
2024-03-20 02:10:12 +0600

    Create NumberMirror.cpp

---
+++ Practice/NumberMirror.cpp
@@ -0,0 +1,17 @@
+                                                                      // Authod: Md.Imran Ali
+
+
+// Problem: Write a program that takes a number N as the input, and prints it to the output.
+
+
+// Solution
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int n;
+    cin >> n;
+    
+    cout<<n;
+}
